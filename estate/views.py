2c07048b31e24da30e0c3ab984bbcbbd88467995
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-02 12:33:02 +0300

    add views for bussiness create post

--- estate/views.py
+++ estate/views.py
@@ -57,19 +57,19 @@ class PostListView(ListView):
     context_object_name='posts'
     ordering='-date_posted'
 
-# class PostCreateView(LoginRequiredMixin,CreateView,UserPassesTestMixin):
-#     model = Post
-#     fields = ['post']
+class PostCreateView(LoginRequiredMixin,CreateView,UserPassesTestMixin):
+    model = Post
+    fields = ['post']
     
-#     def form_valid(self,form):
-#         form.instance.author = self.request.user
-#         return super().form_valid(form)
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-#     def test_func(self):
-#         post = self.get_object()
-#         if self.request.user == post.author:
-#             return True
-#         return False
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
     
 # class PostDeleteView(LoginRequiredMixin,DeleteView,UserPassesTestMixin):
 #     model=Post
