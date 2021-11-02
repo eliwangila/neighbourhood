05e0b046abbf019e7d1bbafef460015c03e6043a
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-02 10:40:42 +0300

    add user form registration

--- user/forms.py
+++ user/forms.py
@@ -10,14 +10,15 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields=['username','email', 'password1','password2']
         
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+           
+# class UserUpdateForm(forms.ModelForm):
+#     email = forms.EmailField()
     
-    class Meta:
-        model = User
-        fields = ['username','email']
+#     class Meta:
+#         model = User
+#         fields = ['username','email']
         
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['image','bio']
+# class ProfileUpdateForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['image','bio']
