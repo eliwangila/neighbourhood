0b6b42d3bef9e52597549d891d63a86629175c43
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-02 10:48:28 +0300

    add update user

--- user/forms.py
+++ user/forms.py
@@ -9,16 +9,16 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields=['username','email', 'password1','password2']
-        
-           
-# class UserUpdateForm(forms.ModelForm):
-#     email = forms.EmailField()
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
     
-#     class Meta:
-#         model = User
-#         fields = ['username','email']
+    class Meta:
+        model = User
+        fields = ['username','email']
         
-# class ProfileUpdateForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields = ['image','bio']
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image','bio']
