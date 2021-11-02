2c21ff0f18fb89b6cfb1b7c8a089a1b4e399ae99
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-02 12:23:26 +0300

    add save method for neighbourhood

--- estate/models.py
+++ estate/models.py
@@ -11,25 +11,25 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length=100,null=True)
     occupant_count = models.IntegerField(null=True)
     
-#     def save_neighborhood(self):
-#         self.save()
+    def save_neighborhood(self):
+        self.save()
         
-#     @classmethod
-#     def delete_neighborhood(cls,id):
-#         cls.objects.filter(id).delete()
+    @classmethod
+    def delete_neighborhood(cls,id):
+        cls.objects.filter(id).delete()
 
-#     @classmethod
-#     def update_neighborhood(cls,id,new_name):
-#         cls.objects.filter(id=id).update(hood_name = 'newname')
+    @classmethod
+    def update_neighborhood(cls,id,new_name):
+        cls.objects.filter(id=id).update(hood_name = 'newname')
    
-#     @classmethod
-#     def update_family_count(cls,id,new_occupant):
-#        cls.objects.filter(id=id).update(family_size = new_occupant) 
+    @classmethod
+    def update_family_count(cls,id,new_occupant):
+       cls.objects.filter(id=id).update(family_size = new_occupant) 
 
-#     @classmethod
-#     def search_hood(cls,id):
-#        search= cls.objects.filter(neighborhood_name__icontains=id)
-#        return search
+    @classmethod
+    def search_hood(cls,id):
+       search= cls.objects.filter(neighborhood_name__icontains=id)
+       return search
     
     
    
