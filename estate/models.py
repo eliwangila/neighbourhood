cd87aad2ad54b788a83df83bcacb8e3486351360
eliwangila <eli.wangila@student.moringaschool.com>
2021-11-02 12:20:34 +0300

    add class post in models

--- estate/models.py
+++ estate/models.py
@@ -63,13 +63,13 @@ class Business(models.Model):
 #     def get_absolute_url(self):
 #         return reverse('business-detail',kwargs={'pk':self.pk})
 
-# class Post(models.Model):
-#     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-#     post = TextField(null=True)
-#     date_posted = models.DateTimeField(auto_now=True)
-#     neighourhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True,blank=True)
+class Post(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    post = TextField(null=True)
+    date_posted = models.DateTimeField(auto_now=True)
+    neighourhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True,blank=True)
 
     
-#     def get_absolute_url(self):
-#         return reverse('post')
+    def get_absolute_url(self):
+        return reverse('post')
     
